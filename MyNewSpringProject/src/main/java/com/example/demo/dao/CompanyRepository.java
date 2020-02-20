333f875c363a3fcba627e8903908145585b0752b
BalajiChevuru81 <60084340+BalajiChevuru81@users.noreply.github.com>
2020-02-20 17:12:58 +0530

    added newSpring file

---
+++ MyNewSpringProject/src/main/java/com/example/demo/dao/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.pojos.Company;
+
+public interface CompanyRepository extends CrudRepository <Company, String> {
+
+}
---
+++ MyNewSpringProject/src/main/java/com/example/demo/dao/StockExchangeDataFieldRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.demo.pojos.StockExchangeDataField;
+
+public interface StockExchangeDataFieldRepository<id3> extends CrudRepository<id3, Integer>{
+
+}
